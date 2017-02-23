c64e7a2a947e364cb48de305c51ef869dee88c19
Mariia_Lapovska <Aafktrcfylhjdyf2806>
2017-02-23 13:18:23 +0200

    add inserts to import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
 INSERT INTO subject (id, name) VALUES (1, 'Math');
 INSERT INTO subject (id, name) VALUES (2, 'English');
 INSERT INTO subject (id, name) VALUES (3, 'Ukrainian');
+INSERT INTO faculty (id, name, info, passing_score, recruitment_plan, is_available) VALUES (1, 'Software engineering', 'This is faculty', 750, 100, 1);
+INSERT INTO faculty_subject(faculty_id, subject_id) VALUES (1, 1);
