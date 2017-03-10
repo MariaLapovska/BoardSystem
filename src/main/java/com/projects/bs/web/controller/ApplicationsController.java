1694d9fde6dfa92f5fcdb88de51363b5cad7ed9a
Mariia_Lapovska <Aafktrcfylhjdyf2806>
2017-03-10 18:35:09 +0200

    rename controller

--- src/main/java/com/projects/bs/web/controller/ApplicationsController.java
+++ src/main/java/com/projects/bs/web/controller/ApplicationsController.java
@@ -2,18 +2,14 @@ package com.projects.bs.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
-/**
- * Created by Mariia_Lapovska on 09-Mar-17.
- */
 @Controller
-public class SomeController {
+public class ApplicationsController {
 
     @GetMapping("/")
-    public String lol(Map<String, Object> model) {
+    public String getApplications(Map<String, Object> model) {
         return "index";
     }
 }
